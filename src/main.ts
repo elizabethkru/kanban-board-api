389db24c2df628e04b30a3b6606038345ad66289
Maxim <maxkryu2004@gmail.com>
2026-05-18 20:08:19 +0300

    add swagger yml

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Response } from 'express';
+import * as YAML from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,17 +13,20 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-    const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('Kanban Board API')
     .setDescription('API для управления канбан-досками (Trello-клон)')
     .setVersion('1.0')
     .addBearerAuth() // добавляем поддержку JWT в Swagger
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); // документация будет доступна по /api-docs
-  
-    
+  app.use('/api-docs-yaml', (req, res: Response) => {
+    res.header('Content-Type', 'application/x-yaml');
+    res.send(YAML.stringify(document));
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 
   logger.log('Application is running on: http://localhost:3000');
